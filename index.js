cf7a1d9b7b074082959299c42713cf98bbabc30c
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-11-09 22:42:14 +0100

    first routes enabled

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import router from './routes/index.js';
 
 const app = express();
-
-
 const port  = process.env.PORT || 4000;
+
+
+
+app.use('/', router);
+
+
 
 
 
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    res.send('Inicio');
+});
+router.get('/nosotros', (req, res)=>{
+    res.send('Nosotros');
+});
+router.get('/contacto', (req, res)=>{
+    res.send('contacto');
+});
+
+
+export default router;
