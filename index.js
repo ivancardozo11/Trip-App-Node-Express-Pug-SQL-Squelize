6e31e1eb10bfdad28bbadfb672a62f5c5b999878
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-11-10 14:00:44 +0100

    add connection string

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import router from './routes/index.js';
+import db from './config/db.js';
+
 
 const app = express();
 const port  = process.env.PORT || 4000;
 
-
-
+db.authenticate().then(()=> console.log('Base de datos conectada')).catch( error => console.log(error));
 app.set('view engine', 'pug');
 
 app.use((req, res, next)=>{
