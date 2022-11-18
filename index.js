0b25ccff2870f168fae5bb65e521f276073218bf
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-11-18 03:00:19 +0100

    Models and controllers created and conected

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import router from './routes/index.js';
 import db from './config/db.js';
 
-console.log(process.env.DB_KEY)
 const app = express();
 const port  = process.env.PORT || 4000;
 
---
+++ models/Viaje.js
@@ -0,0 +1,27 @@
+import Sequelize from 'sequelize';
+import db from '../config/db.js';
+
+export const Viaje = db.define('viajes', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    precio: {
+        type: Sequelize.STRING
+    },
+    fecha_ida: {
+        type: Sequelize.DATE
+    },
+    fecha_vuelta: {
+        type: Sequelize.DATE
+    },
+    imagen: {
+        type: Sequelize.STRING
+    },
+    descripcion: {
+        type: Sequelize.DATE
+    },
+    slug: {
+        type: Sequelize.STRING
+    }
+
+});
