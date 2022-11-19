41331c5cd47063e1a95d92f555e8d665be9fdee5
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-11-19 03:40:19 +0100

    add testimonal route and error handling from form empty spaces

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ import {
     paginaDetalleViaje
 } from '../controllers/paginasController.js';
 
+import {
+    guardarTestimonial
+} from '../controllers/testimonialController.js'
+
 const router = express.Router();
 
 router.get('/', paginaInicio);
@@ -14,6 +18,8 @@ router.get('/nosotros', paginaNosotros);
 router.get('/viajes', paginaViajes);
 router.get('/viajes/:slug', paginaDetalleViaje);
 router.get('/testimoniales', paginaTestimoniales);
+
+router.post('/testimoniales', guardarTestimonial);
 
 
 export default router;
