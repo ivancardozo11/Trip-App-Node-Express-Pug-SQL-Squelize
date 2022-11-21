fde5f44b7dc3a862dfb25148342fe162c8a8622f
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-11-21 05:46:23 +0100

    add testimonial footer version and add npm start for prod

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -3,12 +3,18 @@ import { Testimonial } from '../models/Testimoniales.js'
 
 const paginaInicio = async (req, res) =>{
 
+    const promiseDB = [];
+
+    promiseDB.push(Viaje.findAll({ limit: 3 }));
+    promiseDB.push(Testimonial.findAll({ limit: 3 }));
+
     try { 
-        const viajes = await Viaje.findAll({ limit: 4 });       
+        const resultado = await Promise.all( promiseDB );       
         res.render('inicio',{
             pagina:'Inicio',
             clase: 'home',
-            viajes
+            viajes: resultado[0],
+            testimoniales: resultado[1]
         });
     } catch (error) {
         console.log(error);
